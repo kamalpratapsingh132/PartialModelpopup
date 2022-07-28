b00dcf3359ee29937abbfa5b21d817298934be77
kamalprataps <kamalprataps@CHETUIWK0689>
2022-07-28 16:45:19 +0530

    Add project files.

---
+++ PartialModelpopup/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialModelpopup.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartialModelpopup.Controllers
+{
+    public class UserController : Controller
+    {
+        private ApplicationDBContext _context;
+
+        public UserController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var emp = _context.User.ToList();
+            return View(emp);
+        }
+
+        public ActionResult CreateEdit(int? id)
+        {
+            UserDto model = new UserDto();
+            model.IsActive = true;
+            if (id.HasValue)
+            {
+                var emp = _context.User.Where(e => e.Id == id);
+            }
+            return PartialView("_CreateEdit", model);
+
+        }
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult CreateEdit(UserDto model)
+        {
+            //validate user  
+            if (!ModelState.IsValid)
+            {
+
+
+                return PartialView("_CreateEdit", model);
+            }
+
+            else
+            {
+                _context.User.Add(model);
+                _context.SaveChanges();
+                return RedirectToAction("index");
+
+            }
+            //save user into database   
+            
+        }
+    }
+}
---
+++ PartialModelpopup/Models/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartialModelpopup.Models
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
